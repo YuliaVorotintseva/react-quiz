be988ee5b204c6bd547f9b4acb6ce6908cea5d31
Юлия Воротинцева <yulia.vorotintseva@gmail.com>
2020-04-19 01:53:10 +0600

    add FinishedQuiz

---
+++ src/components/finishedquiz/FinishedQuiz.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styleClasses from './FinishedQuiz.module.scss'
+import Button from '../UI/button/Button'
+
+const FinishedQuiz = props => {
+    const successCount = Object.keys(props.results).reduce((total, key) => {
+        if(props.results[key] === 'success') {
+            total++
+        }
+        return total
+    }, 0)
+
+    return (
+        <div className={styleClasses.FinishedQuiz}>
+            <ul>
+                {props.quiz.map((quizItem, index) => {
+                    const cls = [
+                        'fa',
+                        props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+                        styleClasses[props.results[quizItem.id]]
+                    ]
+
+                    return (
+                        <li key={index}>
+                            <strong>{index + 1}.</strong>&nbsp;
+                            {quizItem.question}
+                            <i className={cls.join(' ')} />
+                        </li>
+                    )
+                })}
+            </ul>
+            <p>Right: {successCount} / {props.quiz.length}</p>
+            <div>
+                <Button onClick={props.onRetry} type='primary'>Again</Button>
+                <Button type='success'>Tests</Button>
+            </div>
+        </div>
+    )
+}
+
+export default FinishedQuiz
