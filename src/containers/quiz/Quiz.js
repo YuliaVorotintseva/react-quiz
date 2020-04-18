adb5782063e4f55270da76dfcb5feff83f2788da
Юлия Воротинцева <yulia.vorotintseva@gmail.com>
2020-04-18 13:37:32 +0600

    update Quiz

--- src/containers/quiz/Quiz.js
+++ src/containers/quiz/Quiz.js
@@ -4,15 +4,27 @@ import ActiveQuiz from '../../components/activequiz/ActiveQuiz'
 
 class Quiz extends Component {
     state = {
-        quiz: []
+        quiz: [
+            {
+                answers: [
+                    {text: "Answer 1"},
+                    {text: "Answer 2"},
+                    {text: "Answer 3"},
+                    {text: "Answer 4"},
+                    {text: "Answer 5"}
+                ]
+            }
+        ]
     }
 
     render() {
         return (
             <div className={styleClasses.Quiz}>
                 <div className={styleClasses.QuizWrapper}>
-                    <h1>Quiz</h1>
-                    <ActiveQuiz />
+                    <h1>Answer the questions</h1>
+                    <ActiveQuiz
+                        answers={this.state.quiz[0].answers}
+                    />
                 </div>
             </div>
         )
