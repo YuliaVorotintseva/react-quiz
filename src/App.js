d689ceea9f9d234ad5b3855aa4a41800552face2
Юлия Воротинцева <yulia.vorotintseva@gmail.com>
2020-04-18 13:01:28 +0600

    update App.js - render Quiz

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Layout from './hoc/layout/Layout'
+import Quiz from './containers/quiz/Quiz'
 
 function App() {
   return (
     <Layout>
-      <div
-        style={{width: 400, border: '1px solid black'}}
-      >
-        <h1>Layout</h1>
-      </div>
+      <Quiz />
     </Layout>
   )
 }
